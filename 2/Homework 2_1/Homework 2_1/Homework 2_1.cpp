5b2b8677dc69839fa5ba655316d5f5cb812fdd99
Diana <gggMissDiggg@mail.ru>
2024-11-20 20:02:19 +0300

    added hw2

---
+++ 2/Homework 2_1/Homework 2_1/Homework 2_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    std::cout << fibonacci(5) << " " << fibonacci(10) << " " << fibonacci(15) << std::endl;
+    return 0;
+}
---
+++ 2/Homework 2_2/Homework 2_2/Homework 2_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int fibonacci(int n)
+{
+    int* memo = new int[n] { 1, 1 };
+    for (int count = 2; count < n; count++)
+    {
+        memo[count] = memo[count - 2] + memo[count - 1];
+    }
+    return memo[n - 1];
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    std::cout << fibonacci(5) << " " << fibonacci(10) << " " << fibonacci(15) << std::endl;
+    return 0;
+}
